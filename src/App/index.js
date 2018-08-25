fd0c88cc8c1b360c504b2a11386bcd291c0c83df
Gorka <gorka@aquigorka.com>
2018-08-25 14:48:17 +0200

    App: update composed view to use blocks store and view

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Web3Provider from '../components/Web3Provider';
-import BlockPolling from '../components/BlockPolling';
+import { Store as BlocksStore, View as BlocksView } from '../components/Blocks';
+import Header from '../components/Header'
 
 class App extends Component {
   render() {
     return (
       <Web3Provider>
-        <BlockPolling>
-          <div>Forscher</div>
-        </BlockPolling>
+        <BlocksStore>
+          <Header />
+          <BlocksView />
+        </BlocksStore>
       </Web3Provider>
     );
   }
