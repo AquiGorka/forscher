7682cc67c7e02733e9e6bf64a4e7135c906239ff
Gorka <gorka@aquigorka.com>
2018-08-25 18:02:33 +0200

    Header: add design styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,36 @@ const Header = styled.header`
   top: 0;
   background: #fff;
   box-shadow: 0 0 0.2rem rgba(0, 0, 0, 0.1), 0 0.2rem 0.4rem rgba(0, 0, 0, 0.2);
-  padding: 1em;
   font-family: 'Oswald', sans-serif;
   font-size: 16px;
+  padding: 1em;
 `;
 
-const H1 = styled.h1`
+const Wrap = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
   max-width: 600px;
   margin: auto;
 `;
 
+const H1 = styled.h1`
+  padding: 0;
+  margin: 0;
+`;
+
+const Legend = styled.div`
+  position: relative;
+  top: -3px;
+`;
+
 export default () => (
   <Header>
-    <H1>Forscher</H1>
+    <Wrap>
+      <H1>Forscher</H1>
+      <Legend>
+        Last update: {new Date().toLocaleString()}
+      </Legend>
+    </Wrap>
   </Header>
 );
