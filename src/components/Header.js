bc538aa9f889c8a5c664a8bbbfeafb5ba4f8bbc7
Gorka <gorka@aquigorka.com>
2018-08-25 15:10:11 +0200

    Header: add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default () => <header>Header</header>
+const Header = styled.header`
+  position: sticky;
+  top: 0;
+  background: #fff;
+  box-shadow: 0 0 0.2rem rgba(0, 0, 0, 0.1), 0 0.2rem 0.4rem rgba(0, 0, 0, 0.2);
+  padding: 1em;
+  font-family: 'Oswald', sans-serif;
+  font-size: 16px;
+`;
+
+const H1 = styled.h1`
+  max-width: 600px;
+  margin: auto;
+`;
+
+export default () => (
+  <Header>
+    <H1>Forscher</H1>
+  </Header>
+);
