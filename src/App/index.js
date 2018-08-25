0867002ce453929e9970a3221a85ec134e4e5ba0
Gorka <gorka@aquigorka.com>
2018-08-25 12:55:10 +0200

    App: update composed view with block polling

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import Web3Provider from '../components/Web3Provider'
+import Web3Provider from '../components/Web3Provider';
+import BlockPolling from '../components/BlockPolling';
 
 class App extends Component {
   render() {
-    return <Web3Provider>
-      <div>Forscher</div>
-    </Web3Provider>;
+    return (
+      <Web3Provider>
+        <BlockPolling>
+          <div>Forscher</div>
+        </BlockPolling>
+      </Web3Provider>
+    );
   }
 }
 
